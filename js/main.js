4d8312f93bb247b8a6b3e722707b53970485d367
nithinmohan <nithinmohan94@gmail.com>
2015-11-28 20:49:11 +0530

    added some commands

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 function getChar(event) {
   if (event.which == null) {
     return String.fromCharCode(event.keyCode) // IE
-  } else if (event.which!=0 && event.charCode!=0) {
+  } else if (event.which!=0 && event.charCode!=0&&!isKey(event,keyCodes.Enter)&&!isKey(event,keyCodes.Backspace)) {
     return String.fromCharCode(event.which)   // the rest
   } else {
-    return null // special key
+    return '' // special key
   }
 }
 var keyCodes={};
 keyCodes.Enter=13;
 keyCodes.Backspace=8;
+var data={
+	available_links:['link1','link2'],
+};
 function isKey(event,keycode){
 	if (event.which == null) {
     return keycode==event.keyCode;// IE
@@ -19,8 +22,29 @@ function isKey(event,keycode){
     return false; // special key
   }
 }
+function _(data){
+	console.log(data);
+}
 function getAnswer(input){
-	return input+" bleh";
+	var init_data='',action;
+	if(input=='clear'){
+		action='clear';
+	}
+	else if(input=='av_links'){
+		action=false;
+		for(var c in data.available_links){
+			init_data=init_data+'<a href="'+data.available_links[c]+'">'+data.available_links[c]+'</a><br>'
+		}
+		output=init_data;
+	}
+	else{
+		output='unknown command';
+		action=false;
+	}
+	return {
+			action:action,
+			output:output
+		}
 }
 function addDiv(data){
 	var iDiv = document.createElement('div');
@@ -32,11 +56,12 @@ app.directive('typingPoint', function() {
   return {
     restrict: 'AEC',
     replace: true,
-    template: '<div>you:{{input}}</div>',
+    template: '<div>you :{{input}}</div>',
     link: function(scope, elem, attrs) {
+    scope.input='';
      document.body.onkeypress=function(e){
         scope.$apply(function() {
-          scope.input=scope.input?scope.input+getChar(e):getChar(e);
+          scope.input=scope.input+getChar(e);
         });
       };
       document.body.onkeydown=function(e){
@@ -47,7 +72,13 @@ app.directive('typingPoint', function() {
      	}
      	else if(isKey(event,keyCodes.Enter)){
      		addDiv("you           :"+scope.input);
-     		addDiv("me            :"+getAnswer(scope.input));
+     		var answer=getAnswer(scope.input);
+     		if(answer.action==false){
+     			addDiv("me            :"+answer.output);
+     		}
+     		else if(answer.action=='clear'){
+     			document.getElementById('prev_chat').innerHTML="";
+     		}
      		scope.$apply(function() {
           		scope.input='';
         	});
